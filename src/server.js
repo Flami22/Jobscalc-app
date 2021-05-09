03d8aa3dc69e026dd4fd7ce28bdd7fc297bf4e0b
João Henrique <joao.henrique-1998@hotmail.com>
2021-05-09 11:21:17 -0300

    V2 refatorado para sqlite

--- src/server.js
+++ src/server.js
@@ -2,10 +2,16 @@ const express = require("express")
 const server = express()
 const routes = require("./routes")
 
-//habilitar arquivos statics
+//usando template engine
+server.set('view engine', 'ejs')
+
+//habilitar arquivos statics da pasta public
 server.use(express.static("public"))
 
-//routes
+// habilitar o uso do req.body
+server.use(express.urlencoded({ extended: true }))
+
+//setado config para o server utilizar routes
 server.use(routes)
 
 server.listen(3000, () => console.log('servidor rodando'))
